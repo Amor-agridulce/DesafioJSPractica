05e9ba02b89bdddf8f67f932d0fb89827407e4d1
Pia <insside.pia@hotmail.com>
2022-04-25 22:06:22 -0400

    Desafio JS en Practica

---
+++ ejercicio1.js
@@ -0,0 +1,37 @@
+const formulario = document.getElementById("formulario");
+const nombre = document.getElementById("nombre");
+const asunto = document.getElementById("asunto");
+const mensaje = document.getElementById("mensaje");
+let resultado = document.getElementsByClassName("resultado");
+let errorNombre = document.getElementsByClassName("errorNombre");
+let errorAsunto = document.getElementsByClassName("errorAsunto");
+let errorMensaje = document.getElementsByClassName("errorMensaje")
+
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(!nombre.value.trim());
+
+errorNombre[0].innerHTML = ""
+errorAsunto[0].innerHTML = ""
+errorMensaje[0].innerHTML = ""
+
+const regExpLetras = /^[a-zA-Z\u00f1\u00d1]\w+/;
+
+
+if (!regExpLetras.test(nombre.value)){
+    errorNombre[0].innerHTML = "Solo se permiten letras"
+}
+
+if (!regExpLetras.test(asunto.value)){
+    errorAsunto[0].innerHTML = "El asunto es requerido"
+    
+}
+if (!regExpLetras.test(mensaje.value)){
+    errorMensaje[0].innerHTML = "El mensaje es requerido"
+    
+}
+if (!regExpLetras.test(nombre.value) || !regExpLetras.test(asunto.value) || !regExpLetras.test(mensaje.value)) return;
+
+    resultado[0].innerHTML = "El mensaje fue enviado con éxito"
+});
---
+++ ejercicio3.js
@@ -0,0 +1,20 @@
+const valor1 = document.getElementById("valor1");
+const valor2 = document.getElementById("valor2");
+
+const btnSumar = document.getElementById("btn-sumar");
+const btnRestar = document.getElementById("btn-restar");
+
+const resultado = document.querySelector(".resultado");
+
+btnSumar.addEventListener("click", () => {
+    let suma = +valor1.value + +valor2.value;
+    resultado.textContent = `${suma}`
+});
+btnRestar.addEventListener("click", () => {
+    let resta = +valor1.value - +valor2.value;
+    if(resta < 0){
+        resultado.textContent = "0"
+        return
+    }
+    resultado.textContent = `${resta}`
+});
